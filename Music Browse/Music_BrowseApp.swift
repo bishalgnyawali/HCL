f8f55b688418d3d0119309f97e0f3bdeb8180d27
Bishal Gnyawali <bishal.gnyawali@gmail.com>
2021-08-30 10:09:24 +1000

    Initial Commit

---
+++ Music Browse/Music_BrowseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_BrowseApp.swift
+//  Music Browse
+//
+//  Created by Bishal on 30/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Music_BrowseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
