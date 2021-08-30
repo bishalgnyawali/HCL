e63b084fc67c12a9887c6810e90ac185c4d4d790
Bishal Gnyawali <bishal.gnyawali@gmail.com>
2021-08-30 17:52:20 +1000

    First Commit

--- Music Browse/ContentView.swift
+++ Music Browse/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("MG")
+                .resizable()
+                .ignoresSafeArea()
+            
+            HStack {
+                Text("Browse Top Ten Music By Artist")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+            }
+            Spacer()
+            
+        }
     }
 }
 
